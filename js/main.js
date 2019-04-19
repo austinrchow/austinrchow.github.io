7b55da4388e7f1bc0ec5f0575be1482967ff90fd
Austin Chow <archow@stanford.edu>
2019-04-18 21:50:09 -0700

    home page animations added

--- js/main.js
+++ js/main.js
@@ -7,3 +7,19 @@ $(function(){
         navMain.collapse('hide');
     });
 });
+
+(function(document){
+  var div = document.getElementById('container');
+  var icon = document.getElementById('icon');
+  var open = false;
+
+  div.addEventListener('click', function(){
+    if(open){
+      icon.className = 'navbar-toggler-icon';
+    } else{
+      icon.className = 'navbar-toggler-icon open';
+    }
+
+    open = !open;
+  });
+})(document);
