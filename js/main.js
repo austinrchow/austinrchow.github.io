2ac6d41be0d671429310c07c81f6a30e2bbc5d83
Austin Chow <archow@stanford.edu>
2019-04-19 04:02:22 -0700

    added resume, portfolio, and photography

--- js/main.js
+++ js/main.js
@@ -23,3 +23,17 @@ $(function(){
     open = !open;
   });
 })(document);
+
+
+$(function() {
+var selectedClass = "";
+$(".filter").click(function(){
+selectedClass = $(this).attr("data-rel");
+$("#gallery").fadeTo(100, 0.1);
+$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+setTimeout(function() {
+$("."+selectedClass).fadeIn().addClass('animation');
+$("#gallery").fadeTo(300, 1);
+}, 300);
+});
+});
